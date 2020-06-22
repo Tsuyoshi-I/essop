2d6974ebee9b9ef61ea3eab8f26cd2c41575955d
tys.iskw <20ishikawa_tsuyoshi@axiz.co.jp>
2020-06-22 14:47:15 +0900

    会員登録画面エラーメッセージ関係実装

---
+++ js/validation.js
@@ -0,0 +1,32 @@
+/**
+ * 入力要素
+ */
+const loginId = document.getElementById("loginId")
+const userName = document.getElementById("userName")
+const tel = document.getElementById("tel")
+const address = document.getElementById("address")
+const pass = document.getElementById("pass")
+const rePass = document.getElementById("rePass")
+
+/**
+ * 正規表現リスト
+ */
+const ONLY_HALF_WIDTH_NUM = "^[0-9]+$"// 半角数字のみ
+const LENGTH_AND_FORMAT_LIMIT = "[!-~]{4,16}" // 4文字以上16文字以下の記号含む半角英数
+
+tel.setAttribute("pattern", ONLY_HALF_WIDTH_NUM)
+pass.setAttribute("pattern", LENGTH_AND_FORMAT_LIMIT)
+rePass.setAttribute("pattern", LENGTH_AND_FORMAT_LIMIT)
+
+tel.addEventListener("input", function () {
+  this.setCustomValidity("")
+  this.checkValidity();
+})
+
+tel.addEventListener("invalid", function () {
+  if (this.value === "") {
+    this.setCustomValidity("入力必須項目です")
+  } else {
+    this.setCustomValidity("半角数字のみ入力してください")
+  }
+})
